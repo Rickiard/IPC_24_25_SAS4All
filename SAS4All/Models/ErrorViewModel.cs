70ab7205aeaddc45c3386266325bbcffdb805965
Rickiard <al79194@alunos.utad.pt>
2025-04-06 00:10:39 +0100

    Criação do projeto;

---
+++ SAS4All/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SAS4All.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
