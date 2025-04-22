1d2c2b8d0c0ff386537d323a1603f651fb90e5fd
Rodrigo Miranda <rodrigo16fm@hotmail.com>
2025-04-22 11:06:50 +0100

    View da MBWay e do pop up de confirmação.

---
+++ SAS4All/Controllers/CarregamentosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SAS4ALL.Models;
+
+namespace SAS4ALL.Controllers
+{
+    public class CarregamentosController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new CarregamentosViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(CarregamentosViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Aqui irias gerar a referência MB Way
+                return RedirectToAction("Sucesso");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Sucesso()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ SAS4All/Models/CarregamentosViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SAS4ALL.Models
+{
+    public class CarregamentosViewModel
+    {
+        [Required]
+        [Phone]
+        [Display(Name = "Número de telemóvel")]
+        public string Telemovel { get; set; }
+
+        [Required]
+        [Range(5, 250)]
+        [Display(Name = "Valor (€)")]
+        public decimal Valor { get; set; }
+    }
+}
+
